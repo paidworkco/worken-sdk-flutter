0856a1eb2fb940dd2f47a44364a44dae1f7a10ad
Alan <32499390+Natusiek@users.noreply.github.com>
2024-02-17 17:49:06 +0100

    setup WorkenSdk

--- lib/worken_sdk.dart
+++ lib/worken_sdk.dart
@@ -1,4 +1,17 @@
 library worken_sdk;
 
+import 'package:http/http.dart';
+import 'package:web3dart/web3dart.dart';
+import 'package:worken_sdk/configuration/dotenv.dart';
+import 'package:worken_sdk/configuration/injectable_config.dart';
+import 'package:worken_sdk/constants/routes.dart';
+
 /// A WorkenSdk.
-class WorkenSdk {}
+class WorkenSdk {
+  static Web3Client client = Web3Client(Routes.rpcUrl, Client());
+
+  void setup() async {
+    configureGetIt();
+    configureDotenv();
+  }
+}
