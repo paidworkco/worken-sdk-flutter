e3411417fa7ad854e5c6b33d79d86636b3b835b1
Alan <32499390+Natusiek@users.noreply.github.com>
2024-02-17 17:48:26 +0100

    setup flutter_dotenv

---
+++ lib/configuration/dotenv.dart
@@ -0,0 +1,6 @@
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+
+void configureDotenv() async => await dotenv.load(fileName: '.env', mergeWith: {
+      'contractAddress': '',
+      'polygonApiKey': '' // todo test this myb remove it
+    });
