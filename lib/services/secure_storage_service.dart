fd3528194241bf7ff5a710e31e21b846dff8b799
Alan <32499390+Natusiek@users.noreply.github.com>
2024-02-17 17:47:28 +0100

    create secure_storage_service with secure_item

---
+++ lib/services/secure_storage_service.dart
@@ -0,0 +1,29 @@
+import 'package:dartz/dartz.dart';
+import 'package:flutter_secure_storage/flutter_secure_storage.dart';
+import 'package:injectable/injectable.dart';
+import 'package:worken_sdk/models/secure_item.dart';
+
+@LazySingleton()
+class SecureStorageService {
+  final FlutterSecureStorage _secureStorage = const FlutterSecureStorage(
+      aOptions: AndroidOptions(encryptedSharedPreferences: true));
+
+  Future<Option<dynamic>> writeSecureData(SecureItem item) async {
+    try {
+      await _secureStorage.write(key: item.id.name, value: item.value);
+      return none();
+    } catch (e) {
+      return some(item); //todo exception or failure
+    }
+  }
+
+  Future<Option<String?>> readSecureData(SecuresId securesId) async {
+    try {
+      final result = await _secureStorage.read(key: securesId.name);
+
+      return some((result != null && result.isNotEmpty) ? result : null);
+    } catch (e) {
+      return none(); //todo exception or failure
+    }
+  }
+}
