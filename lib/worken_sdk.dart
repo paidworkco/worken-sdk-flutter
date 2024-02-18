87fba79325fdd79c2c22ddbea12903fb2ebc77a0
Alan <32499390+Natusiek@users.noreply.github.com>
2024-02-19 00:41:36 +0100

    rebuild workenSdk to impl and add function walletSerivce

--- lib/worken_sdk.dart
+++ lib/worken_sdk.dart
@@ -2,16 +2,22 @@ library worken_sdk;
 
 import 'package:http/http.dart';
 import 'package:web3dart/web3dart.dart';
-import 'package:worken_sdk/configuration/dotenv.dart';
-import 'package:worken_sdk/configuration/injectable_config.dart';
-import 'package:worken_sdk/constants/routes.dart';
+import 'package:worken_sdk/core/configuration/dotenv.dart';
+import 'package:worken_sdk/core/configuration/injectable_config.dart';
+import 'package:worken_sdk/core/constants/routes.dart';
+import 'package:worken_sdk/features/wallet/data/services/wallet_service.dart';
+import 'package:worken_sdk/i_worken_sdk.dart';
 
 /// A WorkenSdk.
-class WorkenSdk {
-  static Web3Client client = Web3Client(Routes.rpcUrl, Client());
+class WorkenSdk extends IWorkenSdk {
+  @override
+  final Web3Client client = Web3Client(Routes.maticvigil, Client());
 
-  void setup() async {
+  WorkenSdk() {
     configureGetIt();
     configureDotenv();
   }
+
+  @override
+  WalletService walletService() => locator.get<WalletService>();
 }
