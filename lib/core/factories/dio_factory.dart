c56a801325e489efcae758d6e86b74cf554a8bcf
Natusiek <32499390+Natusiek@users.noreply.github.com>
2024-02-21 00:16:29 +0100

    new functions on dio_factory (handleException, get)

--- lib/core/factories/dio_factory.dart
+++ lib/core/factories/dio_factory.dart
@@ -1,7 +1,9 @@
 import 'package:dio/dio.dart';
 import 'package:injectable/injectable.dart';
 import 'package:worken_sdk/core/constants/routes.dart';
+import 'package:worken_sdk/core/extensions/response_extension.dart';
 import 'package:worken_sdk/core/factories/i_dio_factory.dart';
+import 'package:worken_sdk/core/models/errors/exceptions.dart';
 
 @LazySingleton(as: IDioFactory)
 class DioFactory extends IDioFactory {
@@ -14,4 +16,29 @@ class DioFactory extends IDioFactory {
       // validateStatus: (int? status) => status! > 0),
     ),
   );
+
+  @override
+  Future<Response> get(String route) async {
+    try {
+      return await dio.get(route);
+    } on DioException catch (e) {
+      throw RestException(e.type, e.message ?? "Bad response");
+    } catch (e) {
+      rethrow;
+    }
+  }
+
+  @override
+  Exception handleException(Object object) {
+    if (object is Response) {
+      if (object.successful()) {
+        throw PolygonException(object.data);
+      }
+      throw ResponseException(
+          code: object.statusCode ?? 0,
+          message: object.statusMessage ?? "Bad response",
+          data: object.data);
+    }
+    throw object;
+  }
 }
--- lib/core/factories/i_dio_factory.dart
+++ lib/core/factories/i_dio_factory.dart
@@ -2,4 +2,8 @@ import 'package:dio/dio.dart';
 
 abstract class IDioFactory {
   abstract Dio dio;
+
+  Future<Response> get(String route);
+
+  Exception handleException(Object object);
 }
