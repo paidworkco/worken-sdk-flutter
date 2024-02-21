4d410937c7df90f53d5f1f1b0ef9268cdd8aecc7
Natusiek <32499390+Natusiek@users.noreply.github.com>
2024-02-21 01:01:12 +0100

    add metod in dotenv and rebuild injectable

--- lib/core/configuration/dotenv.dart
+++ lib/core/configuration/dotenv.dart
@@ -1,12 +1,19 @@
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:worken_sdk/core/constants/constants.dart';
+import 'package:worken_sdk/core/models/errors/exceptions.dart';
 
 Future<void> configureDotenv() async {
-  return await dotenv.load(fileName: '.env', mergeWith: {
+  try {
+    /* mergeWith: {
     Constants.contractAddress: 'YOUR_CONTRACT_ADDRESS',
     Constants.polygonApiKey: 'YOUR_POLYGON_API_KEY'
-  });
+      }*/
+    return await dotenv.load(fileName: '.env');
+  } catch (e) {
+    throw const DotenvException(
+        "WorkenSdk | Create file [.env] to configure sdk");
+  }
 }
 
-//String getPolygonApiKey() => dotenv.get(Constants.polygonApiKey);
-String getContractAddress() => dotenv.get(Constants.contractAddress);
+String get getPolygonApiKey => dotenv.get(Constants.polygonApiKey);
+String get getContractAddress => dotenv.get(Constants.contractAddress);
