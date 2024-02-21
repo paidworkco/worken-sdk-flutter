da8946bf9a24ab99a7617972e5a34864c2cf89b8
Natusiek <32499390+Natusiek@users.noreply.github.com>
2024-02-21 01:02:19 +0100

    injectable config

--- lib/core/configuration/injectable_config.dart
+++ lib/core/configuration/injectable_config.dart
@@ -1,9 +1,17 @@
 import 'package:get_it/get_it.dart';
+import 'package:http/http.dart';
 import 'package:injectable/injectable.dart';
+import 'package:web3dart/web3dart.dart';
+import 'package:worken_sdk/core/constants/routes.dart';
 
 import 'injectable_config.config.dart';
 
 final GetIt locator = GetIt.instance;
 
 @InjectableInit()
-void configureGetIt() => locator.init();
+void configureInjectable() => locator.init();
+
+void configureGetIt() {
+  configureInjectable();
+  locator.registerLazySingleton(() => Web3Client(Routes.maticvigil, Client()));
+}
