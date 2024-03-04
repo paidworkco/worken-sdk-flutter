af86753d5fe05a649010e7b0d9a370dd169d2f59
Natusiek <32499390+Natusiek@users.noreply.github.com>
2024-03-04 15:29:48 +0100

    apply new abi

--- lib/core/constants/abi.dart
+++ lib/core/constants/abi.dart
@@ -1,12 +1,36 @@
 import 'package:web3dart/web3dart.dart';
 
 class Abi {
-  static const erc20balance = '''[
+  static const String erc20balance = '''[
             {
               "constant": true,
               "inputs": [{"name": "_owner", "type": "address"}],
               "name": "balanceOf",
               "outputs": [{"name": "balance", "type": "uint256"}],
+              "type": "function"
+            },
+            {
+              "inputs": [
+                {
+                  "internalType": "address",
+                  "name": "recipient",
+                  "type": "address"
+                },
+                {
+                  "internalType": "uint256",
+                  "name": "amount",
+                  "type": "uint256"
+                }
+              ],
+              "name": "transfer",
+              "outputs": [
+                {
+                  "internalType": "bool",
+                  "name": "",
+                  "type": "bool"
+                }
+              ],
+              "stateMutability": "nonpayable",
               "type": "function"
             }
           ]''';
